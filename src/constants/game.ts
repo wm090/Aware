10ab6c74f4d2be97d275052ffeeb8dfa184627fa
wm090 <wazir.murad02@gmail.com>
2025-04-26 23:09:26 +0200

    feat: Update player color and adjust arrow speed constants for improved gameplay

--- src/constants/game.ts
+++ src/constants/game.ts
@@ -1,18 +1,18 @@
 // Game constants
 export const PLAYER = {
   RADIUS: 20,
-  COLOR: 'white',
+  COLOR: 'black',
   INITIAL_POSITION: { x: 0, y: 0 }, // Will be centered in the screen
 };
 
 export const ARROW = {
   SIZE: 15,
-  MIN_SPEED: 4,  // Increased from 2
-  MAX_SPEED: 6, // Increased from 5
+  MIN_SPEED: 7,  // Increased from 2
+  MAX_SPEED: 8, // Increased from 5
   SPAWN_INTERVAL: 1000, // ms
   MAX_COUNT: 20, // Maximum number of arrows on screen
 };
 
 export const GAME = {
-  BACKGROUND_COLOR: 'black',
+  BACKGROUND_COLOR: 'white',
 };
